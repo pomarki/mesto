899fb9c947914928edefdc04def9992e391aa349
Denis Shalaev <pomarki@yandex.ru>
2021-02-02 14:04:12 +0300

    после ревью №2-01

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,15 @@ let popupWindow = document.querySelector(".popup");
 let profileContainer = document.querySelector(".profile");
 let profileName = profileContainer.querySelector(".profile__name");
 let profileJob = profileContainer.querySelector(".profile__job");
-let formUserInfo = document.querySelector('form[name=user-info]');
-let formUserName = formUserInfo.querySelector('input[name=user-name]');
-let formUserJob = formUserInfo.querySelector('input[name=user-job]');
+let formUserInfo = document.querySelector("form[name=user-info]");
+let formUserName = formUserInfo.querySelector("input[name=user-name]");
+let formUserJob = formUserInfo.querySelector("input[name=user-job]");
 let buttonPopupClose = popupWindow.querySelector(".popup__close");
 
 function openEditPopup() {
   popupWindow.classList.add("popup_opened");
   formUserName.value = profileName.textContent;
-  formUserJob.value =  profileJob.textContent;
-  console.log(formUserName);
+  formUserJob.value = profileJob.textContent;
 }
 function closeEditPopup() {
   popupWindow.classList.remove("popup_opened");
@@ -22,7 +21,7 @@ function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = formUserName.value;
   profileJob.textContent = formUserJob.value;
-  
+
   closeEditPopup();
 }
 
