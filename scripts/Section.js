f9b3e162dc09fd60b0815b980408c43d76dd9f58
Denis Shalaev <pomarki@yandex.ru>
2021-03-28 21:16:29 +0300

    начал работу над спринтом 07

---
+++ scripts/Section.js
@@ -0,0 +1,6 @@
+// items — это массив данных, которые нужно добавить на страницу при инициализации класса
+// renderer — это функция, которая отвечает за создание и отрисовку данных на странице.
+export class Section {
+    constructor({items, renderer}, formSelector) {}
+    addItem() {}
+}
---
+++ scripts/UserInfo.js
@@ -0,0 +1,9 @@
+
+export class UserInfo {
+    constructor({name, job}) {
+    
+    }
+    getUserInfo() {} // возвращает объект с данными пользователя
+    setUserInfo() {} // принимает новые данные пользователя и добавляет их на страницу
+
+}
